fd61122af1cc9385b8a754f836cf0d0bea096db5
Sören Steiger <soren1@gmx.net>
2021-08-27 16:30:09 +0200

    Update default text color

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Header() {
   return (
     <>
-      <h1>
-        <b>TALISMOONS</b>
+      <h1 style={{ marginTop: '10px' }}>
+        <b style={{ color: '#fff', fontSize: '23px' }}>TALISMOONS</b>
       </h1>
     </>
   )
