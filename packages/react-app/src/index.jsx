13cf9b5e722478b48bfbbb94804c786496967019
Sören Steiger <soren1@gmx.net>
2021-09-18 12:04:29 +0200

    Fix issue with browsers blocking theme import

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -5,24 +5,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.less'
 
-/* TODO:
 const themes = {
   dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
   light: `${process.env.PUBLIC_URL}/light-theme.css`
-}
-*/
-
-/*
-const PUBLIC_URL = 'http://localhost:3000'
-const themes = {
-  dark: `${PUBLIC_URL}/dark-theme.css`,
-  light: `${PUBLIC_URL}/light-theme.css`
-}
-*/
-
-const themes = {
-  dark: 'http://localhost:3000/dark-theme.css',
-  light: 'http://localhost:3000/light-theme.css'
 }
 
 // const prevTheme = window.localStorage.getItem('theme')
