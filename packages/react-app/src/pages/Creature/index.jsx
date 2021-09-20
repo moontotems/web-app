02db20221c24743276cd86467f9710ec9b24e3dd
Sören Steiger <soren1@gmx.net>
2021-09-20 16:41:40 +0200

    Creature page: Scroll to top on load

--- packages/react-app/src/pages/Creature/index.jsx
+++ packages/react-app/src/pages/Creature/index.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import CreaturePageDesktop from './desktop'
 import CreaturePageMobile from './mobile'
 
 export default function CreaturePage({ ethereumProps, nftAppProps }) {
   const { isMobile } = nftAppProps
+
+  // scroll to top on load
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   return (
     <>
