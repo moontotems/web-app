a29cbf0ab054acfac1b3ec4facdf34ac9a197aba
Sören Steiger <soren1@gmx.net>
2021-09-30 12:15:33 +0200

    Smart contracts: Add base uri parameter to constructor

--- packages/hardhat/contracts/mocks/nf-token-metadata-enumerable-mock.sol
+++ packages/hardhat/contracts/mocks/nf-token-metadata-enumerable-mock.sol
@@ -14,10 +14,12 @@ contract NFTokenMetadataEnumerableMock is NFTokenEnumerable, NFTokenMetadata, Ow
    * @dev Contract constructor.
    * @param _name A descriptive name for a collection of NFTs.
    * @param _symbol An abbreviated name for NFTokens.
+   * @param _nftBaseUri A base prefix for all token uris.
    */
-  constructor(string memory _name, string memory _symbol) {
+  constructor(string memory _name, string memory _symbol, string memory _nftBaseUri) {
     nftName = _name;
     nftSymbol = _symbol;
+    nftBaseUri = _nftBaseUri;
   }
 
   /**
--- packages/hardhat/deploy/00_deploy_nf-token-metadata-enumerable-mock_contract.js
+++ packages/hardhat/deploy/00_deploy_nf-token-metadata-enumerable-mock_contract.js
@@ -1,12 +1,16 @@
 // const { utils } = require("ethers");
 
+const nftName = "Moons";
+const nftSymbol = "MOON";
+const nftBaseUri = "http://13.82.82.15:5000/api/token/";
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
   await deploy("NFTokenMetadataEnumerableMock", {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    args: ["CryptoMoons", "CMOON"],
+    args: [nftName, nftSymbol, nftBaseUri],
     log: true,
   });
 };
