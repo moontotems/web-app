9d884d547a1e57932d7c465b8fe85ace6356daff
Sören Steiger <soren1@gmx.net>
2021-10-03 15:06:08 +0200

    Update design of Attributes page

--- packages/react-app/src/pages/Attributes/index.jsx
+++ packages/react-app/src/pages/Attributes/index.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'antd'
-import { MoonColumn } from './components'
 
 export default function AttributesPage({ ethereumProps, nftAppProps }) {
-  const { route, setRoute } = nftAppProps
+  // scroll to top on load
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
 
   const cardStyles = {
+    wrapper: {
+      position: 'absolute',
+      width: '350px',
+      padding: '15px'
+    },
     container: {
-      width: '20%',
       backgroundColor: 'rgb(0,0,0, 0.6)'
     },
     title: {
@@ -17,6 +22,7 @@ export default function AttributesPage({ ethereumProps, nftAppProps }) {
       fontSize: '20px'
     },
     content: {
+      height: '130px',
       padding: '10px',
       paddingBottom: '30px',
       fontSize: '16px'
@@ -26,98 +32,123 @@ export default function AttributesPage({ ethereumProps, nftAppProps }) {
     <>
       <Row>
         <Col xs={24} md={5}>
-          <div style={{ padding: '20px', fontSize: '20px' }}>
+          <div style={{ padding: '40px', fontSize: '25px' }}>
             Characteristics
           </div>
         </Col>
         <Col xs={24} md={10}>
-          <div style={{ padding: '20px', fontSize: '25px', fontWeight: 300 }}>
+          <div style={{ padding: '40px', fontSize: '30px', fontWeight: 300 }}>
             Unique DNA combination of attributes
           </div>
         </Col>
       </Row>
-      <div></div>
-      <Row
-        style={{
-          background:
-            'url(/attributes/talismoons_attributes.jpg) no-repeat center center fixed',
-          backgroundSize: 'cover',
-          paddingTop: '40%',
-          paddingBottom: '40%'
-        }}
-      >
-        <div style={{ float: 'left', width: '100%' }}>
+      <Row>
+        <Col xs={24}>
+          <img
+            src='/attributes/talismoons_attributes.jpg'
+            style={{ float: 'right', width: '100%' }}
+          />
+
           <div
             style={{
-              float: 'left',
-              ...cardStyles.container
-              //marginTop: '40%'
+              ...cardStyles.wrapper,
+              bottom: 500,
+              left: 0
             }}
           >
-            <img
-              src='/attributes/talismonns_attributes_eyes.jpg'
-              style={{ width: '100%' }}
-            />
-            <div style={{ ...cardStyles.title }}>Eyes</div>
-            <div style={{ ...cardStyles.content }}>
-              Eyes are the window to the soul. Totem eyes have different shapes.
-              In rare cases Totems can have asymmetrical or even multicolor
-              eyes.
+            <div
+              style={{
+                ...cardStyles.container
+              }}
+            >
+              <img
+                src='/attributes/talismonns_attributes_eyes.jpg'
+                style={{ width: '100%' }}
+              />
+              <div style={{ ...cardStyles.title }}>Eyes</div>
+              <div style={{ ...cardStyles.content }}>
+                Eyes are the window to the soul. Totem eyes have different
+                shapes. In rare cases Totems can have asymmetrical or even
+                multicolor eyes.
+              </div>
             </div>
           </div>
+
           <div
             style={{
-              float: 'right',
-              ...cardStyles.container
-              //marginTop: '40%'
+              ...cardStyles.wrapper,
+              bottom: 500,
+              right: 0
             }}
           >
-            <img
-              src='/attributes/talismoons_attributes_materiality.jpg'
-              style={{ width: '100%' }}
-            />
-            <div style={{ ...cardStyles.title }}>Materiality</div>
-            <div style={{ ...cardStyles.content }}>
-              Eyes are the window to the soul. Totem eyes have different shapes.
-              In rare cases Totems can have asymmetrical or even multicolor
-              eyes.
+            <div
+              style={{
+                ...cardStyles.container
+              }}
+            >
+              <img
+                src='/attributes/talismoons_attributes_complexity.jpg'
+                style={{ width: '100%' }}
+              />
+              <div style={{ ...cardStyles.title }}>Complexity</div>
+              <div style={{ ...cardStyles.content }}>
+                Eyes are the window to the soul. Totem eyes have different
+                shapes. In rare cases Totems can have asymmetrical or even
+                multicolor eyes.
+              </div>
             </div>
           </div>
-        </div>
-        <div style={{ float: 'left', width: '100%' }}>
+
           <div
             style={{
-              float: 'left',
-              ...cardStyles.container
+              ...cardStyles.wrapper,
+              bottom: 'calc(500px - 510px)',
+              left: 0
             }}
           >
-            <img
-              src='/attributes/talismonns_attributes_child_moons.jpg'
-              style={{ width: '100%' }}
-            />
-            <div style={{ ...cardStyles.title }}>Child Moon</div>
-            <div style={{ ...cardStyles.content }}>
-              Eyes are the window to the soul. In rare cases Totems can have
-              asymmetrical or even multicolor eyes.
+            <div
+              style={{
+                ...cardStyles.container
+              }}
+            >
+              <img
+                src='/attributes/talismoons_attributes_materiality.jpg'
+                style={{ width: '100%' }}
+              />
+              <div style={{ ...cardStyles.title }}>Materiality</div>
+              <div style={{ ...cardStyles.content }}>
+                Eyes are the window to the soul. Totem eyes have different
+                shapes. In rare cases Totems can have asymmetrical or even
+                multicolor eyes.
+              </div>
             </div>
           </div>
+
           <div
             style={{
-              float: 'right',
-              ...cardStyles.container
+              ...cardStyles.wrapper,
+              bottom: 'calc(500px - 510px)',
+              right: 0
             }}
           >
-            <img
-              src='/attributes/talismoons_attributes_complexity.jpg'
-              style={{ width: '100%' }}
-            />
-            <div style={{ ...cardStyles.title }}>Complexity</div>
-            <div style={{ ...cardStyles.content }}>
-              Eyes are the window to the soul. In rare cases Totems can have
-              asymmetrical or even multicolor eyes.
+            <div
+              style={{
+                ...cardStyles.container
+              }}
+            >
+              <img
+                src='/attributes/talismonns_attributes_child_moons.jpg'
+                style={{ width: '100%' }}
+              />
+              <div style={{ ...cardStyles.title }}>Child Moon</div>
+              <div style={{ ...cardStyles.content }}>
+                Eyes are the window to the soul. Totem eyes have different
+                shapes. In rare cases Totems can have asymmetrical or even
+                multicolor eyes.
+              </div>
             </div>
           </div>
-        </div>
+        </Col>
       </Row>
     </>
   )
