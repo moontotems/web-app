614503af099d1f8e4e73be62a7d89087d994ba67
Sören Steiger <soren1@gmx.net>
2021-10-14 22:12:09 +0200

    Update assets for home page

--- packages/react-app/src/pages/Home/desktop/components/LunarMonths/slideContents.js
+++ packages/react-app/src/pages/Home/desktop/components/LunarMonths/slideContents.js
@@ -1,79 +1,79 @@
 export default [
   {
     index: 0,
-    image: '/home/lunarMonths/512/01_ghost_moon.jpg',
+    image: '/home/lunarMonths/lunarMonths.gif',
     title: '',
     text: 'Each Talismoon is born under a particular Lunar Month.'
   },
   {
     index: 1,
-    image: '/home/lunarMonths/512/01_ghost_moon.jpg',
+    image: '/home/lunarMonths/svg/01_ghost_moon.svg',
     title: 'Ghost Moon',
     text: 'Season of ancestry, remembering those who came before us, now living in our memories.'
   },
   {
     index: 2,
-    image: '/home/lunarMonths/512/02_snow_moon.jpg',
+    image: '/home/lunarMonths/svg/02_snow_moon.svg',
     title: 'Snow Moon',
     text: 'Crystalize fluid situations'
   },
   {
     index: 3,
-    image: '/home/lunarMonths/512/03_crow_moon.jpg',
+    image: '/home/lunarMonths/svg/03_crow_moon.svg',
     title: 'Crow Moon',
     text: 'Opportunists, harbringers, and observers'
   },
   {
     index: 4,
-    image: '/home/lunarMonths/512/04_fish_moon.jpg',
+    image: '/home/lunarMonths/svg/04_fish_moon.svg',
     title: 'Fish Moon',
     text: 'Comfortable under pressure'
   },
   {
     index: 5,
-    image: '/home/lunarMonths/512/05_milk_moon.jpg',
+    image: '/home/lunarMonths/svg/05_milk_moon.svg',
     title: 'Milk Moon',
     text: 'Fluid and maternal'
   },
   {
     index: 6,
-    image: '/home/lunarMonths/512/06_honey_moon.jpg',
+    image: '/home/lunarMonths/svg/06_honey_moon.svg',
     title: 'Honey Moon',
     text: 'Collective effort'
   },
   {
     index: 7,
-    image: '/home/lunarMonths/512/07_thunder_moon.jpg',
+    image: '/home/lunarMonths/svg/07_thunder_moon.svg',
     title: 'Thunder Moon',
     text: 'Energetic, shocking'
   },
   {
     index: 8,
-    image: '/home/lunarMonths/512/08_buck_moon.jpg',
+    image: '/home/lunarMonths/svg/08_buck_moon.svg',
     title: 'Buck Moon',
     text: 'Easily provoked, horny'
   },
   {
     index: 9,
-    image: '/home/lunarMonths/512/09_harvest_moon.jpg',
+    image: '/home/lunarMonths/svg/09_harvest_moon.svg',
     title: 'Harvest Moon',
     text: 'Abundant'
   },
   {
     index: 10,
-    image: '/home/lunarMonths/512/10_leaf_moon.jpg',
+    image: '/home/lunarMonths/svg/10_leaf_moon.svg',
     title: 'Leaf Moon',
     text: 'Traveller, nomadic'
   },
   {
     index: 11,
-    image: '/home/lunarMonths/512/11_blood_moon.jpg',
+    image: '/home/lunarMonths/svg/11_blood_moon.svg',
     title: 'Blood Moon',
     text: 'Surrounded by three concentric rings of mountains, uplifted by the colossal impact event that excavated it.'
   },
   {
     index: 12,
-    image: '/home/lunarMonths/512/12_oak_moon.jpg',
+    image: '/home/lunarMonths/svg/12_oak_moon.svg',
     title: 'Oak Moon',
     text: 'Static, stoic, stable, solid'
   }
--- packages/react-app/src/pages/Home/desktop/components/LunarPhases/slideContents.js
+++ packages/react-app/src/pages/Home/desktop/components/LunarPhases/slideContents.js
@@ -1,55 +1,55 @@
 export default [
   {
     index: 0,
-    image: '/home/lunarPhases/512/02_waxing_crescent.jpg',
+    image: '/home/lunarPhases/lunarPhases.gif',
     title: '',
     text: 'Each Talismoon is born under a particular Lunar Phase.'
   },
   {
     index: 1,
-    image: '/home/lunarPhases/512/01_new_moon.jpg',
+    image: '/home/lunarPhases/svg/01_new_moon.svg',
     title: 'New Moon',
     text: 'New Begging, Virgninal'
   },
   {
     index: 2,
-    image: '/home/lunarPhases/512/02_waxing_crescent.jpg',
+    image: '/home/lunarPhases/svg/02_waxing_crescent.svg',
     title: 'Waxing Crescent',
     text: 'Youth, open'
   },
   {
     index: 3,
-    image: '/home/lunarPhases/512/03_last_quarter.jpg',
+    image: '/home/lunarPhases/svg/03_last_quarter.svg',
     title: 'First Quarter',
     text: 'Surrounded by three concentric rings of mountains, uplifted by the colossal impact event that excavated it.'
   },
   {
     index: 4,
-    image: '/home/lunarPhases/512/04_waxing_gibbous.jpg',
+    image: '/home/lunarPhases/svg/04_waxing_gibbous.svg',
     title: 'Waxing Gibbous',
     text: 'Exhuberance, excitement, fertile'
   },
   {
     index: 5,
-    image: '/home/lunarPhases/512/05_full_moon.jpg',
+    image: '/home/lunarPhases/svg/05_full_moon.svg',
     title: 'Full Moon',
     text: 'Pinnacle, peak, climax'
   },
   {
     index: 6,
-    image: '/home/lunarPhases/512/06_waning_gibbous.jpg',
+    image: '/home/lunarPhases/svg/06_waning_gibbous.svg',
     title: 'Waning Gibbous',
     text: 'The Day after, Reflection'
   },
   {
     index: 7,
-    image: '/home/lunarPhases/512/07_last_quarter.jpg',
+    image: '/home/lunarPhases/svg/07_last_quarter.svg',
     title: 'Last Quarter',
     text: 'Last Hurrah, Realization Dawning of the Mind'
   },
   {
     index: 8,
-    image: '/home/lunarPhases/512/08_waning_crescent.jpg',
+    image: '/home/lunarPhases/svg/08_waning_crescent.svg',
     title: 'Waning Crescent',
     text: 'Old, Wise, Calm, Enlightenment'
   }
