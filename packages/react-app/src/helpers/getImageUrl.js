671efaa3940e60b11df08801112310145a037034
Sören Steiger <soren1@gmx.net>
2021-10-18 17:20:49 +0200

    Update image base url

--- packages/react-app/src/helpers/getImageUrl.js
+++ packages/react-app/src/helpers/getImageUrl.js
@@ -2,7 +2,8 @@ import getTokenPrefixZeros from './getTokenPrefixZeros'
 
 export default function getImageUrl(tokenId) {
   //const image = 'https://talismoonstest.blob.core.windows.net/creatures/MOONTOTEMS_9282.jpeg'
-  const image = `/creatures/2048/MOONTOTEMS_${tokenId}.jpeg`
+  //const image = `/creatures/2048/MOONTOTEMS_${tokenId}.jpeg`
+  const image = `https://moontotems.blob.core.windows.net/totems/base/jpeg/2048/moontotems_g1_base_2048_${tokenId}.jpg`
   //const image = `https://talismoonstest.blob.core.windows.net/images/512/MOONTOTEMS_${tokenId}.jpeg`
   //const image = `https://talismoonstest.blob.core.windows.net/finalrenders200kb/MOONTOTEMS_1k_${prefixedTokenId}.jpeg`
   //const image = `https://talismoonstest.blob.core.windows.net/finalrenders/TALISMOONS_GEN01_2k${prefixedTokenId}.png`
