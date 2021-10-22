84b9c20b5b700988c4b4f981436db35038133b55
Sören Steiger <soren1@gmx.net>
2021-10-22 18:39:03 +0200

    Update smart contract deployment paramters

--- packages/hardhat/deploy/00_deploy_nft-talismoons_contract.js
+++ packages/hardhat/deploy/00_deploy_nft-talismoons_contract.js
@@ -1,24 +1,15 @@
-// const { utils } = require("ethers");
-
-/*
-// const CONTRACT_NAME = "Talismoons"
-
-const nftName = "Talismoons";
-const nftSymbol = "TMOON";
-const nftBaseUri = "http://13.82.82.15:5000/api/token/";
-*/
-
 const CONTRACT_NAME = "MoonTotems";
 
 const nftName = "MoonTotems";
-const nftSymbol = "MOONT";
-const nftBaseUri = "http://13.82.82.15:5000/api/token/";
+const nftSymbol = "TOTEM";
+const nftBaseUri = "https://api.moontotems.com/token/";
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
   console.log("now deploying contract: " + CONTRACT_NAME);
+
   await deploy(CONTRACT_NAME, {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
