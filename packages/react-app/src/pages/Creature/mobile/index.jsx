64903486f9e3cc4806982913c99e206c99fa8f51
Sören Steiger <soren1@gmx.net>
2021-10-24 13:27:45 +0200

    Adjust mobile creature page to more match the desktop version

--- packages/react-app/src/pages/Creature/mobile/index.jsx
+++ packages/react-app/src/pages/Creature/mobile/index.jsx
@@ -9,20 +9,25 @@ import {
 } from '@carbon/icons-react'
 // https://www.npmjs.com/package/react-slick
 import { useSwipeable } from 'react-swipeable'
-import $ from 'jquery'
 
 import { MIN_TOKEN_ID, MAX_TOKEN_ID } from '../../../constants'
 import { getImageUrl } from '../../../helpers'
 import { creatureFeatures } from '../../../sharedComponents'
 const { MetaData, Chatbot, FileDownloads, WriteStory, FreshMintMessage } =
   creatureFeatures
-import houdini_json_hashmap from '../../../assets/houdini_json_hashmap.json'
 
 export default function CreaturesMobileView({ ethereumProps, nftAppProps }) {
   const { address } = ethereumProps
-  const { favorites, mintEventsMap, mint } = nftAppProps
+  const {
+    assembleCreature,
+    creatures,
+    filter: { activeFilters },
+    mintEventsMap,
+    mint,
+    favorites
+  } = nftAppProps
 
-  const { checkIfIsFavorite, updateFavorites } = favorites
+  const { updateFavorites } = favorites
 
   const updateUrl = creatureIndex => {
     window.history.replaceState(
@@ -39,70 +44,58 @@ export default function CreaturesMobileView({ ethereumProps, nftAppProps }) {
     urlTokenId = 0
   }
 
-  const [activeTokenId, setActiveTokenId] = useState(urlTokenId)
-
-  useEffect(() => {
-    window.scrollTo(0, 0)
-    $('#chatbot').hide()
-    $('#creatureMetaData').hide()
-  }, [])
-
-  const getNextTokenId = ({ direction }) => {
-    if (typeof activeTokenId !== 'number') {
-      return MIN_TOKEN_ID
-    }
-
-    let newActiveTokenId
-    if (direction === 'left') {
-      if (activeTokenId > MIN_TOKEN_ID) {
-        newActiveTokenId = activeTokenId - 1
-        return newActiveTokenId
-      } else {
-        return MIN_TOKEN_ID
+  // find index position of creature with tokenId == urlTokenId
+  const fetchCreatureIndex = () => {
+    let index
+    creatures.filtered.filter((creature, _index) => {
+      if (creature.tokenId === urlTokenId) {
+        index = _index
       }
-    }
-    if (direction === 'right') {
-      if (activeTokenId === MAX_TOKEN_ID) {
-        return activeTokenId
-      } else {
-        newActiveTokenId = activeTokenId + 1
-        return newActiveTokenId
-      }
-    }
+    })
+    return index
   }
+  const index = fetchCreatureIndex()
 
-  // pre-load 10 images
+  const [visibleCreatureListIndex, setVisibleCreatureListIndex] =
+    useState(index)
+
   useEffect(() => {
-    const preloadSize = 10
-    for (
-      let preloadId = activeTokenId + 1;
-      preloadId < preloadSize + 1;
-      preloadId++
-    ) {
+    if (!creatures.filtered[visibleCreatureListIndex])
+      setVisibleCreatureListIndex(fetchCreatureIndex())
+  }, [activeFilters])
+
+  const currentVisibleCreature = creatures.filtered[visibleCreatureListIndex]
+
+  useEffect(() => {
+    updateUrl(creatures.filtered[visibleCreatureListIndex].tokenId)
+  }, [visibleCreatureListIndex])
+
+  // pre-load 10 images to the left and to the right
+  useEffect(() => {
+    const preloadSize = 10 // in each direction
+
+    const _currentVisibleCreature = creatures.filtered[visibleCreatureListIndex]
+
+    const activeTokenId = _currentVisibleCreature.tokenId
+
+    let startTokenId = MIN_TOKEN_ID
+    if (activeTokenId - preloadSize > MIN_TOKEN_ID) {
+      startTokenId = activeTokenId - preloadSize
+    }
+
+    let endTokenId = MAX_TOKEN_ID
+    if (activeTokenId + preloadSize < MAX_TOKEN_ID) {
+      endTokenId = activeTokenId + preloadSize
+    }
+
+    for (let preloadId = startTokenId; preloadId <= endTokenId; preloadId++) {
       const img = new Image()
-      img.src = getImageUrl({ tokenId: preloadId, size: 1024 })
+      img.src = getImageUrl({ tokenId: preloadId, size: 2048 })
     }
-  }, [activeTokenId])
-
-  // TODO: why are we doing this?? all the data should be available and passed down in/from App.jsx
-  const assembleCreature = tokenId => {
-    const minted = !!mintEventsMap[tokenId]
-    const isFavorite = checkIfIsFavorite(tokenId)
-    const metaData = houdini_json_hashmap[tokenId]
-    const image = getImageUrl({ tokenId, size: 1024 })
-
-    const creature = {
-      tokenId,
-      metaData,
-      image,
-      isFavorite,
-      minted
-    }
-    return creature
-  }
+  }, [visibleCreatureListIndex])
 
   const { tokenId, metaData, image, ownedByUser, minted, isFavorite } =
-    assembleCreature(urlTokenId)
+    currentVisibleCreature || assembleCreature(urlTokenId)
 
   const isAvailable = !minted
   const isTaken = !isAvailable
@@ -115,18 +108,10 @@ export default function CreaturesMobileView({ ethereumProps, nftAppProps }) {
     trackTouch: true,
     //onSwiped: eventData => console.log('User Swiped!', eventData),
     onSwipedLeft: eventData => {
-      const newActiveTokenId = getNextTokenId({
-        direction: 'right'
-      })
-      setActiveTokenId(newActiveTokenId)
-      updateUrl(newActiveTokenId)
+      setVisibleCreatureListIndex(visibleCreatureListIndex - 1)
     },
     onSwipedRight: eventData => {
-      const newActiveTokenId = getNextTokenId({
-        direction: 'left'
-      })
-      setActiveTokenId(newActiveTokenId)
-      updateUrl(newActiveTokenId)
+      setVisibleCreatureListIndex(visibleCreatureListIndex + 1)
     }
     //onTap: ({ event }) =>
   })
@@ -214,14 +199,18 @@ export default function CreaturesMobileView({ ethereumProps, nftAppProps }) {
                 <Favorite32
                   role='button'
                   style={{ fill: 'white', cursor: 'pointer' }}
-                  onClick={() => updateFavorites(activeTokenId)}
+                  onClick={() =>
+                    updateFavorites(currentVisibleCreature.tokenId)
+                  }
                 />
               )}
               {isFavorite && (
                 <FavoriteFilled32
                   role='button'
                   style={{ fill: '#DA1E28', cursor: 'pointer' }}
-                  onClick={() => updateFavorites(activeTokenId)}
+                  onClick={() =>
+                    updateFavorites(currentVisibleCreature.tokenId)
+                  }
                 />
               )}
             </div>
@@ -269,7 +258,7 @@ export default function CreaturesMobileView({ ethereumProps, nftAppProps }) {
                   backgroundColor: '#24A148',
                   borderColor: '#24A148'
                 }}
-                onClick={() => mint(activeTokenId)}
+                onClick={() => mint(currentVisibleCreature.tokenId)}
               >
                 Summon this Totem (0.1 Ξ)
               </Button>
