5946f54432d46ae8fe0be6a6f869025d625f072a
Sören Steiger <soren1@gmx.net>
2021-10-27 19:53:58 +0200

    Fix showing creature features to owner

--- packages/react-app/src/sharedComponents/ActionSidebar/index.jsx
+++ packages/react-app/src/sharedComponents/ActionSidebar/index.jsx
@@ -6,16 +6,20 @@ import ActionSidebarMobile from './mobile'
 export default function ActionSidebar({ ethereumProps, nftAppProps }) {
   const { isMobile, usersCreaturesTokenIds, route } = nftAppProps
 
-  const getNumberFromString = str => {
-    return str.replace(/[^0-9]/g, '')
+  const checkIfShouldShowCreatureFeatures = () => {
+    // const getNumberFromString = str =>  str.replace(/[^0-9]/g, '')
+    const href = window.location.href
+    let hrefWithoutPort = href.substring(href.indexOf('moontotem/') + 10)
+    if (!hrefWithoutPort.length) hrefWithoutPort = href
+
+    const currentCreatureToken = parseInt(hrefWithoutPort)
+    const creatureIsOwnedByUser =
+      usersCreaturesTokenIds.includes(currentCreatureToken)
+
+    return href.includes('moontotem/') && creatureIsOwnedByUser
   }
 
-  const currentCreatureToken = parseInt(getNumberFromString(route))
-  const creatureIsOwnedByUser =
-    usersCreaturesTokenIds.includes(currentCreatureToken)
-
-  const showCreatureFeatures =
-    route.includes('moontotem/') && creatureIsOwnedByUser
+  const showCreatureFeatures = checkIfShouldShowCreatureFeatures()
 
   return (
     <>
