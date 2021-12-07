ffcbf81599adce34919009ab215376994244ed5d
Sören Steiger <soren1@gmx.net>
2021-12-07 12:36:03 +0100

    Update mint script

--- packages/hardhat/scripts/mint.js
+++ packages/hardhat/scripts/mint.js
@@ -1,17 +1,19 @@
 const opensea = require("opensea-js");
+
 const OpenSeaPort = opensea.OpenSeaPort;
 const Network = opensea.Network;
-const MnemonicWalletSubprovider = require("@0x/subproviders")
-  .MnemonicWalletSubprovider;
+const MnemonicWalletSubprovider =
+  require("@0x/subproviders").MnemonicWalletSubprovider;
 const RPCSubprovider = require("web3-provider-engine/subproviders/rpc");
 const Web3ProviderEngine = require("web3-provider-engine");
 
-const MNEMONIC = "round keen claim erode vapor armor cost noise stool dirt exist muscle"; //process.env.MNEMONIC;
-const NODE_API_KEY = "cae33c762d9f4f96b8c083a0b0f70b70"; //process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
+const MNEMONIC =
+  "round keen claim erode vapor armor cost noise stool dirt exist muscle"; // process.env.MNEMONIC;
+const NODE_API_KEY = "cae33c762d9f4f96b8c083a0b0f70b70"; // process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
 const isInfura = !!process.env.INFURA_KEY;
-const FACTORY_CONTRACT_ADDRESS = "0x85dA7825386CB9149a8AE55fb61A2b0f993D526F" //process.env.FACTORY_CONTRACT_ADDRESS;
-const OWNER_ADDRESS = "0xCEd746f0Dda38f251c21574dDdD2bC9D663ca8D4" //process.env.OWNER_ADDRESS;
-const NETWORK = "rinkeby" //process.env.NETWORK;
+const FACTORY_CONTRACT_ADDRESS = "0xfDD9a08C9b5801E532630B4005122B53a8654866"; // process.env.FACTORY_CONTRACT_ADDRESS;
+const OWNER_ADDRESS = "0xCEd746f0Dda38f251c21574dDdD2bC9D663ca8D4"; // process.env.OWNER_ADDRESS;
+const NETWORK = "rinkeby"; // process.env.NETWORK;
 const API_KEY = process.env.API_KEY || ""; // API key is optional but useful if you're doing a high volume of requests.
 
 const DUTCH_AUCTION_OPTION_ID = "1";
@@ -27,12 +29,12 @@ if (!MNEMONIC || !NODE_API_KEY || !NETWORK || !OWNER_ADDRESS) {
   console.error(
     "Please set a mnemonic, Alchemy/Infura key, owner, network, API key, nft contract, and factory contract address."
   );
-  return;
+  // return;
 }
 
 if (!FACTORY_CONTRACT_ADDRESS) {
   console.error("Please specify a factory contract address.");
-  return;
+  // return;
 }
 
 const BASE_DERIVATION_PATH = `44'/60'/0'/0`;
@@ -67,6 +69,7 @@ const seaport = new OpenSeaPort(
 );
 
 async function main() {
+  /*
   // Example: many fixed price auctions for a factory option.
   console.log("Creating fixed price auctions...");
   const fixedSellOrders = await seaport.createFactorySellOrders({
@@ -80,29 +83,37 @@ async function main() {
     startAmount: FIXED_PRICE,
     numberOfOrders: NUM_FIXED_PRICE_AUCTIONS,
   });
+  console.log({ fixedSellOrders })
   console.log(
     `Successfully made ${fixedSellOrders.length} fixed-price sell orders! ${fixedSellOrders[0].asset.openseaLink}\n`
   );
+  */
 
-  /*
   // Example: many fixed price auctions for multiple factory options.
   console.log("Creating fixed price auctions...");
+  console.log({
+    FACTORY_CONTRACT_ADDRESS,
+    OWNER_ADDRESS,
+  });
   const fixedSellOrdersTwo = await seaport.createFactorySellOrders({
     assets: [
-      { tokenId: "3", tokenAddress: FACTORY_CONTRACT_ADDRESS },
-      { tokenId: "4", tokenAddress: FACTORY_CONTRACT_ADDRESS },
-      { tokenId: "5", tokenAddress: FACTORY_CONTRACT_ADDRESS },
-      { tokenId: "6", tokenAddress: FACTORY_CONTRACT_ADDRESS },
+      { tokenId: "10", tokenAddress: FACTORY_CONTRACT_ADDRESS },
+      { tokenId: "11", tokenAddress: FACTORY_CONTRACT_ADDRESS },
+      { tokenId: "12", tokenAddress: FACTORY_CONTRACT_ADDRESS },
+      { tokenId: "13", tokenAddress: FACTORY_CONTRACT_ADDRESS },
     ],
     factoryAddress: FACTORY_CONTRACT_ADDRESS,
     accountAddress: OWNER_ADDRESS,
     startAmount: FIXED_PRICE,
+    endAmount: FIXED_PRICE,
+    expirationTime: Math.round(Date.now() / 1000 + 60 * 60 * 24),
     numberOfOrders: NUM_FIXED_PRICE_AUCTIONS,
   });
   console.log(
-    `Successfully made ${fixedSellOrdersTwo.length} fixed-price sell orders for multiple assets at once! ${fixedSellOrders[0].asset.openseaLink}\n`
+    `Successfully made ${fixedSellOrdersTwo.length} fixed-price sell orders for multiple assets at once! ${fixedSellOrdersTwo[0].asset.openseaLink}\n`
   );
 
+  /*
   // Example: many declining Dutch auction for a factory.
   console.log("Creating dutch auctions...");
 
