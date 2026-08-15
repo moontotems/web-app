79f957d8ec79a669ce19443bbafaabcc049e02b5
Sören Steiger <soren1@gmx.net>
2026-08-15 23:32:25 +0200

    Update sidebar

--- apps/web/src/lib/components/nft/SidebarLeft.tsx
+++ apps/web/src/lib/components/nft/SidebarLeft.tsx
@@ -1,3 +1,4 @@
+import { Drawer, DrawerContent, DrawerDescription, DrawerTitle } from '@moontotems/ui'
 import { Link } from '@tanstack/react-router'
 import {
   BarChart3,
@@ -36,83 +37,85 @@ function MenuItem({
   )
 }
 
-/** 250px slide-over navigation drawer (legacy SidebarLeft). */
+/** 250px left navigation drawer (animated via @moontotems/ui Drawer / vaul). */
 export function SidebarLeft() {
   const { sidebarLeftOpen, setSidebarLeftOpen } = useMoonTotems()
-
-  if (!sidebarLeftOpen) return null
 
   const close = () => setSidebarLeftOpen(false)
   const iconClass = 'size-4'
 
   return (
-    <div
-      id="sidebarLeft"
-      className="fixed left-0 z-1000 w-[250px] overflow-y-auto border-t border-[#393939]"
-      style={{ top: HEADER_HEIGHT - 1 }}
-    >
-      <div
-        className="w-full bg-[#262626] text-white"
-        style={{ height: `calc(100vh - ${HEADER_HEIGHT}px)` }}
+    <Drawer open={sidebarLeftOpen} onOpenChange={setSidebarLeftOpen} direction="left">
+      <DrawerContent
+        id="sidebarLeft"
+        className="nft-theme dark !fixed !top-[39px] !right-auto !bottom-0 !left-0 z-1000 !h-[calc(100vh-39px)] !w-[250px] !max-w-[250px] overflow-y-auto rounded-none border-0 !border-t !border-[#393939] bg-transparent p-0 shadow-none data-[vaul-drawer-direction=left]:!w-[250px] data-[vaul-drawer-direction=left]:!max-w-[250px] data-[vaul-drawer-direction=left]:!border-r-0 data-[vaul-drawer-direction=left]:sm:!max-w-[250px]"
       >
-        <Link to="/" onClick={close}>
-          <MenuItem icon={<Compass className={iconClass} />}>Explore all</MenuItem>
-        </Link>
-        <Link to="/orbit" onClick={close}>
-          <MenuItem icon={<Moon className={iconClass} />}>Orbit</MenuItem>
-        </Link>
-        <Link to="/all" onClick={close}>
-          <MenuItem icon={<Smile className={iconClass} />}>All Moon Totems</MenuItem>
-        </Link>
+        <DrawerTitle className="sr-only">Navigation</DrawerTitle>
+        <DrawerDescription className="sr-only">Moon Totems site menu</DrawerDescription>
 
-        <MenuItem title>About</MenuItem>
-        <Link to="/project-overview" onClick={close}>
-          <MenuItem icon={<Smile className={iconClass} />}>What are Moon Totems?</MenuItem>
-        </Link>
-        <Link to="/attributes" onClick={close}>
-          <MenuItem icon={<CircleUserRound className={iconClass} />}>Unique Characters</MenuItem>
-        </Link>
-        <MenuItem icon={<Lightbulb className={iconClass} />}>Exclusive Features</MenuItem>
-        <Link to="/lunar-origins" onClick={close}>
-          <MenuItem icon={<MapPin className={iconClass} />}>Lunar Origins</MenuItem>
-        </Link>
-        <Link to="/lunar-calendar" onClick={close}>
-          <MenuItem icon={<BarChart3 className={iconClass} />}>Lunar Months</MenuItem>
-        </Link>
-        <Link to="/lunar-calendar" onClick={close}>
-          <MenuItem icon={<Moon className={iconClass} />}>Lunar Phases</MenuItem>
-        </Link>
+        <div
+          className="w-full bg-[#262626] text-white"
+          style={{ height: `calc(100vh - ${HEADER_HEIGHT}px)` }}
+        >
+          <Link to="/" onClick={close}>
+            <MenuItem icon={<Compass className={iconClass} />}>Explore all</MenuItem>
+          </Link>
+          <Link to="/orbit" onClick={close}>
+            <MenuItem icon={<Moon className={iconClass} />}>Orbit</MenuItem>
+          </Link>
+          <Link to="/all" onClick={close}>
+            <MenuItem icon={<Smile className={iconClass} />}>All Moon Totems</MenuItem>
+          </Link>
 
-        <MenuItem title>Latest News</MenuItem>
-        <a href={SOCIAL_LINKS.instagram} target="_blank" rel="noreferrer">
-          <MenuItem icon={<Instagram className={iconClass} />}>Instagram</MenuItem>
-        </a>
-        <a href={SOCIAL_LINKS.twitter} target="_blank" rel="noreferrer">
-          <MenuItem icon={<Twitter className={iconClass} />}>Twitter</MenuItem>
-        </a>
-        <a href={SOCIAL_LINKS.discord} target="_blank" rel="noreferrer">
-          <MenuItem icon={<img src="/icons/Logo-Discord.svg" alt="" className="h-4 w-4" />}>
-            Discord
-          </MenuItem>
-        </a>
+          <MenuItem title>About</MenuItem>
+          <Link to="/project-overview" onClick={close}>
+            <MenuItem icon={<Smile className={iconClass} />}>What are Moon Totems?</MenuItem>
+          </Link>
+          <Link to="/attributes" onClick={close}>
+            <MenuItem icon={<CircleUserRound className={iconClass} />}>Unique Characters</MenuItem>
+          </Link>
+          <MenuItem icon={<Lightbulb className={iconClass} />}>Exclusive Features</MenuItem>
+          <Link to="/lunar-origins" onClick={close}>
+            <MenuItem icon={<MapPin className={iconClass} />}>Lunar Origins</MenuItem>
+          </Link>
+          <Link to="/lunar-calendar" onClick={close}>
+            <MenuItem icon={<BarChart3 className={iconClass} />}>Lunar Months</MenuItem>
+          </Link>
+          <Link to="/lunar-calendar" onClick={close}>
+            <MenuItem icon={<Moon className={iconClass} />}>Lunar Phases</MenuItem>
+          </Link>
 
-        <MenuItem title>NFT Tracking</MenuItem>
-        <a href={SOCIAL_LINKS.opensea} target="_blank" rel="noreferrer">
-          <MenuItem icon={<img src="/icons/Logo-OpenSea.svg" alt="" className="h-4 w-4" />}>
-            Explore on OpenSea
-          </MenuItem>
-        </a>
-        <a href={SOCIAL_LINKS.looksrare} target="_blank" rel="noreferrer">
-          <MenuItem icon={<img src="/icons/Logo-OpenSea.svg" alt="" className="h-4 w-4" />}>
-            Explore on LooksRare
-          </MenuItem>
-        </a>
-        <a href={SOCIAL_LINKS.etherscan} target="_blank" rel="noreferrer">
-          <MenuItem icon={<img src="/icons/Logo-Etherscan.svg" alt="" className="h-4 w-4" />}>
-            Explore on Etherscan
-          </MenuItem>
-        </a>
-      </div>
-    </div>
+          <MenuItem title>Latest News</MenuItem>
+          <a href={SOCIAL_LINKS.instagram} target="_blank" rel="noreferrer">
+            <MenuItem icon={<Instagram className={iconClass} />}>Instagram</MenuItem>
+          </a>
+          <a href={SOCIAL_LINKS.twitter} target="_blank" rel="noreferrer">
+            <MenuItem icon={<Twitter className={iconClass} />}>Twitter</MenuItem>
+          </a>
+          <a href={SOCIAL_LINKS.discord} target="_blank" rel="noreferrer">
+            <MenuItem icon={<img src="/icons/Logo-Discord.svg" alt="" className="h-4 w-4" />}>
+              Discord
+            </MenuItem>
+          </a>
+
+          <MenuItem title>NFT Tracking</MenuItem>
+          <a href={SOCIAL_LINKS.opensea} target="_blank" rel="noreferrer">
+            <MenuItem icon={<img src="/icons/Logo-OpenSea.svg" alt="" className="h-4 w-4" />}>
+              Explore on OpenSea
+            </MenuItem>
+          </a>
+          <a href={SOCIAL_LINKS.looksrare} target="_blank" rel="noreferrer">
+            <MenuItem icon={<img src="/icons/Logo-OpenSea.svg" alt="" className="h-4 w-4" />}>
+              Explore on LooksRare
+            </MenuItem>
+          </a>
+          <a href={SOCIAL_LINKS.etherscan} target="_blank" rel="noreferrer">
+            <MenuItem icon={<img src="/icons/Logo-Etherscan.svg" alt="" className="h-4 w-4" />}>
+              Explore on Etherscan
+            </MenuItem>
+          </a>
+        </div>
+      </DrawerContent>
+    </Drawer>
   )
 }
